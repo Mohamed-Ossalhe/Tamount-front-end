f56f0d8e2db0358c2071be4174ed81918c4c313a
Mohamed-Ossalhe <ossalhemohamed2@gmail.com>
2024-03-11 10:24:34 +0000

    🧱 - add main layouts and add main layouts routes and gitignore config files

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{
+		path: '',
+		loadComponent: () =>
+			import('@layouts/client-layout/client-layout.component').then(
+				(c) => c.ClientLayoutComponent
+			),
+	},
+	{
+		path: 'admin',
+		loadComponent: () =>
+			import('@layouts/admin-layout/admin-layout.component').then(
+				(c) => c.AdminLayoutComponent
+			),
+	},
+];
