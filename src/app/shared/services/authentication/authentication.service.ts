c109df1e6a26dfceb3495df2e3e58de65d4eba77
Mohamed-Ossalhe <ossalhemohamed2@gmail.com>
2024-03-18 12:23:43 +0000

    🤩 - add authentication service

---
+++ src/app/shared/services/authentication/authentication.service.ts
@@ -0,0 +1,52 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '@environments/environment.development';
+import { Observable } from 'rxjs';
+import { AuthenticationResponse } from '@models/authentication-response';
+import { AuthenticationRequest } from '@interfaces/requests/authentication-request';
+import { RegistrationRequest } from '@interfaces/requests/registration-request';
+
+/**
+ * Represents the user authentication service.
+ *
+ * @author Mohamed Ossalhe
+ */
+@Injectable({
+	providedIn: 'root',
+})
+export class AuthenticationService {
+	private authenticateEndPoint: string = environment.API_URL + '/authenticate';
+	private registrationEndPoint: string = environment.API_URL + '/register';
+
+	constructor(private http: HttpClient) {}
+
+	/**
+	 * Authenticates the user using the provided authentication request.
+	 *
+	 * @param authenticationRequest The authentication request containing user credentials.
+	 * @returns An Observable emitting an AuthenticationResponse upon successful authentication.
+	 */
+	public authenticate(
+		authenticationRequest: AuthenticationRequest
+	): Observable<AuthenticationResponse> {
+		return this.http.post<AuthenticationResponse>(
+			this.authenticateEndPoint,
+			authenticationRequest
+		);
+	}
+
+	/**
+	 * Register a new user using the provided registration request.
+	 *
+	 * @param registrationRequest The authentication request containing user credentials.
+	 * @returns An Observable emitting an AuthenticationResponse upon successful authentication.
+	 */
+	public register(
+		registrationRequest: RegistrationRequest
+	): Observable<AuthenticationResponse> {
+		return this.http.post<AuthenticationResponse>(
+			this.registrationEndPoint,
+			registrationRequest
+		);
+	}
+}
